6f1dfc47850317f85c6a192e09eeb4c809c68bdd
EliasS0p0rt3Web <SL71748619@idat.pe>
2026-07-23 20:05:42 -0500

    implementacion de el animacion al abrir la app

--- app/src/main/java/pe/edu/idat/clinicasanmiguel/MainActivity.kt
+++ app/src/main/java/pe/edu/idat/clinicasanmiguel/MainActivity.kt
@@ -1,42 +1,20 @@
 package pe.edu.idat.clinicasanmiguel
 
-import android.animation.AnimatorSet
-import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.animation.DecelerateInterpolator
-import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var imgLogo: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-        imgLogo = findViewById(R.id.imgLogo)
-
-        imgLogo.scaleX = 0.75f
-        imgLogo.scaleY = 0.75f
-        imgLogo.alpha = 0.4f
-
-        val scaleX = ObjectAnimator.ofFloat(imgLogo, "scaleX", 0.75f, 1.0f)
-        val scaleY = ObjectAnimator.ofFloat(imgLogo, "scaleY", 0.75f, 1.0f)
-        val fadeIn = ObjectAnimator.ofFloat(imgLogo, "alpha", 0.4f, 1.0f)
-
-        AnimatorSet().apply {
-            playTogether(scaleX, scaleY, fadeIn)
-            duration = 1200
-            interpolator = DecelerateInterpolator()
-            start()
-        }
 
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, LoginActivity::class.java)
